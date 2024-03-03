8d0543cf113d15bea9ec524bb680f10ee40cdfde
olayafgon <olayafolgueiras@gmail.com>
2024-03-03 23:16:10 +0100

    Add configurations

---
+++ config.py
@@ -0,0 +1,9 @@
+# ARCHIVO DE CONFIGURACIÓN DEL PROYECTO
+
+# GENERAL
+RESULTS_FOLDER = r'.\..\results' # Ruta relativa a la carpeta donde se guardan los resultados y logs
+
+# DESCARGA DE DATOS DE AFLOWLIB
+DATA_DOWNLOAD = True # True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
+DATA_FOLDER_PATH = r'.\..\data' # Ruta relativa a la carpeta donde se guardan los datos
+STRUCTURES = ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] # Redes de Bravais sobre las que se desea trabajar
