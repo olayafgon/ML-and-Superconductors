ebe67e5e4923db7f3dbfeadb5136f499f6a6048e
olayafgon <olayafolgueiras@gmail.com>
2024-03-04 22:55:13 +0100

    Docstring and decompress_data function

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ RESULTS_FOLDER = r'.\..\results' # Ruta relativa a la carpeta donde se guardan l
 DATA_DOWNLOAD = True # True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
 DATA_FOLDER_PATH = r'.\..\data' # Ruta relativa a la carpeta donde se guardan los datos
 STRUCTURES = ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] # Redes de Bravais sobre las que se desea trabajar
+DATA_FILE_TYPE = 'DOSCAR.static.xz' # Tipo de archivo tal como aparece en Aflowlib
--- src/processes/data_download.py
+++ src/processes/data_download.py
@@ -1,44 +1,61 @@
-'''
-This module define a workflow to download data from Aflowlib (http://aflowlib.duke.edu).
+"""
+This module defines a workflow to download data from Aflowlib (http://aflowlib.duke.edu).
 
 Requires some configurations from config.py:
     - STRUCTURES
     - DATA_FOLDER_PATH
-'''
+    - DATA_FILE_TYPE
+
+Example:
+    import config
+    from data_download_module import data_download
+
+    # Initialize the data download module
+    downloader = data_download(run_results_path)
+
+    # Start the data download workflow
+    downloader.data_download_workflow()
+"""
 
 import os
 import sys
 import subprocess
 import requests
 import urllib.request
+import lzma
+import shutil
+import time
 from utils import tools
+
 
 sys.path.append('./../')
 import config
 
-class data_download():
-
+class data_download:
     def __init__(self, run_results_path):
-        '''
-        Inicializate module.
-        '''
+        """
+        Initialize the data download module.
+
+        Args:
+            run_results_path (str): The path where the results and logs are stored.
+        """
         self.run_results_path = run_results_path
         self.structures = config.STRUCTURES
         self.data_folder_path = config.DATA_FOLDER_PATH
         self.data_dowload_check = config.DATA_DOWNLOAD
         self.aflowlib_link = 'http://aflowlib.duke.edu/AFLOWDATA/ICSD_WEB/'
-
+        self.file_type = config.DATA_FILE_TYPE
 
     def get_names(self):
-        '''
-        Creates a folder in data folder with files containing the names of all the materials available in aflowlib for each bravais lattice.
-        '''
+        """
+        Create a folder in the data folder with files containing the names of all the materials available in Aflowlib for each bravais lattice.
+        """
         # Creates folder
         materials_names_folder = os.path.join(self.data_folder_path, 'materials_names')
         tools.create_folder(materials_names_folder)
     
         for structure in self.structures:
-            # Download info files from librery
+            # Download info files from library
             response = requests.get(self.aflowlib_link + structure)
             lines = response.text.split("\n")
 
@@ -58,26 +75,25 @@ class data_download():
                 for line in modified_lines:
                     f_out.write(line)
 
-
     def download_compressed_data(self):
-        # TODO: Añadir oocion para elegir archivo a descargar
-        # TODO: Añadir checjk de errores en la descarga
-        
+        """
+        Download compressed data files from Aflowlib.
+        """
         materials_names_folder = os.path.join(self.data_folder_path, "materials_names")
         data_compressed_folder = os.path.join(self.data_folder_path, "data_compressed")
 
         for structure_name in self.structures:
-            # Directorio de salida
+            # Output directory
             output_directory = os.path.join(data_compressed_folder, structure_name)
             tools.create_folder(output_directory)
 
-            # Para cada estructura XXX sustituir por:  f=Data_names.2.Modified/XXX.m.dat
+            # For each structure
             entry_file = os.path.join(materials_names_folder, f'{structure_name}_names.txt')
 
-            # Bucle para la descarga de los archivos
+            # Download loop
             with open(entry_file, "r") as file:
                 for line in file:
-                    # Eliminar el carácter de nueva línea de la línea
+                    # Remove newline character
                     line = line.strip()
                     
                     j = 0
@@ -89,23 +105,66 @@ class data_download():
                     aa = structure_name
                     bb = line[0:j]
                     
-                    url = f'http://aflowlib.duke.edu/AFLOWDATA/ICSD_WEB/{aa}/{bb}/DOSCAR.static.xz'
+                    url = f'{self.aflowlib_link}{aa}/{bb}/{self.file_type}'
                     file_path = os.path.join(output_directory, f"{aa}_{bb}.xz")
                     
-                    # Imprime la URL antes de descargar
-                    print(f"Descargando: {url}")
+                    # Print URL before downloading
+                    print(f"Downloading: {url}")
 
-                    # Descarga el archivo desde la URL
+                    # Download file from URL
                     urllib.request.urlretrieve(url, file_path)
 
-
-
-
     def decompress_data(self):
-        # TODO: 
-        pass
+        """
+        Decompresses files from the source directory to the destination directory.
+        """
+        # Path to the directory containing the compressed files
+        directorio_origen = os.path.join(self.data_folder_path, "data_compressed")
 
+        # Path to the directory where to decompress the files
+        directorio_destino = os.path.join(self.data_folder_path, "data_raw")
 
+        # Path to the .txt file to save names of files that cannot be decompressed
+        archivo_error = os.path.join(directorio_destino, "00_Errors.txt")
+
+        # Speed limit in bytes per second
+        limite_velocidad = 500 * 1024 * 1024
+
+        # Verifies if the source directory exists
+        if not os.path.exists(directorio_origen):
+            print(f'Error: The source directory "{directorio_origen}" does not exist.')
+            exit()
+
+        # Verifies if the destination directory exists, if not, creates it
+        if not os.path.exists(directorio_destino):
+            os.makedirs(directorio_destino)
+
+        # Loops through all folders and subfolders in the source directory
+        for carpeta, subcarpetas, archivos in os.walk(directorio_origen):
+            for archivo_comprimido in archivos:
+                if archivo_comprimido.endswith('.xz'):
+                    # Constructs input and output paths
+                    ruta_entrada = os.path.join(carpeta, archivo_comprimido)
+                    carpeta_destino = carpeta.replace(directorio_origen, '').lstrip(os.path.sep)
+                    ruta_salida = os.path.join(directorio_destino, carpeta_destino, archivo_comprimido.replace('.xz', ''))
+
+                    # Creates the destination folder if it does not exist
+                    os.makedirs(os.path.join(directorio_destino, carpeta_destino), exist_ok=True)
+
+                    try:
+                        # Decompresses the file limiting the speed
+                        with lzma.open(ruta_entrada, 'rb') as f_in, open(ruta_salida, 'wb') as f_out:
+                            start_time = time.time()
+                            shutil.copyfileobj(f_in, f_out, length=limite_velocidad)
+                            elapsed_time = time.time() - start_time
+                            print(f'Decompressed: {ruta_entrada} (Time: {elapsed_time:.2f} s)')
+                    except Exception as e:
+                        with open(archivo_error, 'a') as error_file:
+                            error_file.write(f'Error in {ruta_entrada}: {str(e)}\n')
+                        print(f'Error in {ruta_entrada}: {str(e)}')
+                        continue  # Skips to the next file if there is an error
+
+        print('Process completed.')
 
     def data_download_workflow(self):
         """
@@ -113,20 +172,23 @@ class data_download():
 
         This function checks the existence of required folders and guides the user through the data download process.
         It prompts the user for input based on the existing folders and performs appropriate actions accordingly.
-
-        Args:
-            self: Instance of the class containing necessary attributes.
-
-        Returns:
-            None
         """
         # Construct paths to required folders
         materials_names_folder = os.path.join(self.data_folder_path, "materials_names")
         data_compressed_folder = os.path.join(self.data_folder_path, "data_compressed")
         data_raw_folder = os.path.join(self.data_folder_path, "data_raw")
 
-        # Complete workflow to dowload data
+        # Complete workflow to download data
         def whole_download_workflow():
+            """
+            Perform the complete workflow to download data.
+
+            This function guides the user through the entire data download process.
+            It prompts the user for confirmation before starting the download process and performs the following steps:
+                1. Retrieve names of materials available in Aflowlib.
+                2. Download compressed data files from Aflowlib.
+                3. Decompress downloaded data files.
+            """
             confirm = input('WARNING: You are about to start downloading files, this can take up to several hours. Are you sure you want to continue? (y/n): ')
             if confirm.lower() == 'y':
                 print("  · Starting download. Please, be patient.")
@@ -235,4 +297,3 @@ class data_download():
         # None exist
         else:
             whole_download_workflow()
-
