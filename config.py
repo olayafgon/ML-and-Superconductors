1c8dbadd6536a2b69eb8c9f0589d6929b7dbe432
olayafgon <olayafolgueiras@gmail.com>
2024-03-12 22:17:28 +0100

    Fixed data dowload on false

--- config.py
+++ config.py
@@ -4,8 +4,7 @@
 RESULTS_FOLDER = r'.\..\results' # Ruta relativa a la carpeta donde se guardan los resultados y logs
 
 # DESCARGA DE DATOS DE AFLOWLIB
-DATA_DOWNLOAD = True # True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
+DATA_DOWNLOAD = False # True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
 DATA_FOLDER_PATH = r'.\..\data' # Ruta relativa a la carpeta donde se guardan los datos
-STRUCTURES = ["BCC"] # Redes de Bravais sobre las que se desea trabajar
-# ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"]
+STRUCTURES = ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] # Redes de Bravais sobre las que se desea trabajar
 DATA_FILE_TYPE = 'DOSCAR.static.xz' # Tipo de archivo tal como aparece en Aflowlib
