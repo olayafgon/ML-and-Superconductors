ebe0acc1e1c05a69973fd4cd358da7369086e94d
olayafgon <olayafolgueiras@gmail.com>
2024-04-22 22:27:18 +0200

    Configs

--- config.py
+++ config.py
@@ -4,9 +4,13 @@
 RESULTS_FOLDER = r'.\..\results' # Ruta relativa a la carpeta donde se guardan los resultados y logs
 
 # DESCARGA DE DATOS DE AFLOWLIB
-DATA_DOWNLOAD = False # True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
-DATA_FOLDER_PATH = r'.\..\data' # Ruta relativa a la carpeta donde se guardan los datos
-STRUCTURES = ["FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] # Redes de Bravais sobre las que se desea trabajar
-# "BCC", "BCT", "CUB", 
+# True si se quieren descargar los datos, False si ya están descargados (deberán estar en DATA_FOLDER_PATH)
+DATA_DOWNLOAD = False 
+# Velocidad límite de descompresión
+SPEED_LIMIT = 2 * 500 * 1024 * 1024 # 2*500Mb/s
+# Ruta relativa a la carpeta donde se guardan los datos
+DATA_FOLDER_PATH = r'.\..\data' 
+# Redes de Bravais sobre las que se desea trabajar
+STRUCTURES = ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] 
 # Tipo de archivo en Aflowlib ('DOSCAR.static.xz' o '_dosdata.json.xz')
 DATA_FILE_TYPE = '_dosdata.json.xz'
