855d6820bcf378acdb70c70be2e50ffcd4bbfd5d
olayafgon <olayafolgueiras@gmail.com>
2024-04-22 22:43:16 +0200

    Added fermi interval limit to config

--- config.py
+++ config.py
@@ -14,3 +14,5 @@ DATA_FOLDER_PATH = r'.\..\data'
 STRUCTURES = ["BCC", "BCT", "CUB", "FCC", "HEX", "MCL", "MCLC", "ORC", "ORCC", "ORCF", "ORCI", "RHL", "TET", "TRI"] 
 # Tipo de archivo en Aflowlib ('DOSCAR.static.xz' o '_dosdata.json.xz')
 DATA_FILE_TYPE = '_dosdata.json.xz'
+# Limites de energía respecto a Fermi en la lectura
+EFERMI_LIMIT = 15
