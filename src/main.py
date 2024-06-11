52b5634d7ecd518cfe2395d1ba7439899066a208
olayafgon <olayafolgueiras@gmail.com>
2024-06-11 21:10:46 +0200

    Added new modules

--- src/main.py
+++ src/main.py
@@ -1,23 +1,15 @@
-'''
-
-'''
-
-import os
 import sys
 import time
-from datetime import datetime
 
 from data_handling import data_raw_download, data_raw_read, data_processing
-from visualizer import eda
+from data_analysis import plotter, reporter
 from utils import tools
-
 
 sys.path.append('./../')
 import config
 
 _DATA_DOWNLOAD = config.DATA_DOWNLOAD
 _READ_DATA_RAW = config.READ_DATA_RAW
-
 
 def main():
 
@@ -42,15 +34,15 @@ def main():
     materials_data, supercon_data = MaterialsProcessor.processor()
 
     #EDA
-    plotter = eda.Plotter(materials_data, run_results_path)
-    plotter.workflow()
+    stats_reporter = reporter.StatsReporter(materials_data, run_results_path) 
+    stats_reporter.stats_report()
 
+    plotter_instance = plotter.Plotter(materials_data, run_results_path)
+    plotter_instance.workflow()
 
     #END
     end_ = time.time()
     tools.log_main(f'Total runtime {(end_-start_):.2f}s ({(end_-start_)/60:.0f}min)', save_path=run_results_path)
-
-    
 
 if __name__ == '__main__':
     main()
