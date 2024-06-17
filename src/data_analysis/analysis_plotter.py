7bdb22560aecb667067fbb4455004c2c27067c8f
olayafgon <olayafolgueiras@gmail.com>
2024-06-17 21:02:04 +0200

    Name change

--- src/data_analysis/analysis_plotter.py
+++ src/data_analysis/analysis_plotter.py
@@ -1,10 +1,7 @@
 import sys
-import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
-
-pd.set_option('display.max_columns', None)
 
 from data_analysis import analysis_utils
 sys.path.append('./../')
