e9e2981493d96178d40c52ce2de348bb1494712f
olayafgon <olayafolgueiras@gmail.com>
2024-07-07 15:35:02 +0200

    Added preprocessor module

--- src/models/hiperparameter_search/hiperameter_exploration.py
+++ src/models/hiperparameter_search/hiperameter_exploration.py
@@ -20,10 +20,10 @@ import config
 from utils import tools
 
 class HiperparameterExploration:
-    def __init__(self, materials_data, run_results_path, method):
-        self.materials_data = materials_data
+    def __init__(self, run_results_path, method, Data_Processor):
         self.run_results_path = run_results_path
         self.method = method
+        self.Data_Processor = Data_Processor
         self.target_column = config.TARGET_COLUMN
         self.eval_metric = config.EVAL_METRIC
         self.n_iter = config.HIPERPARAMETER_EXPLO_ITER
@@ -44,46 +44,6 @@ class HiperparameterExploration:
             'colsample_bytree': [0.01, 0.1, 0.3, 0.5, 0.7, 0.85, 1.0]
             }
         tools.log_main(f'  - Method: {self.method}', save_path=self.run_results_path)
-
-    def process_data(self):
-        self.data = self.materials_data.copy()
-        self.data.dropna(inplace=True)
-        self.data['is_superconductor'] = self.data['is_superconductor'].astype(bool)
-    
-    def define_columns(self):
-        start_col = f"DOS_m{abs(config.EFERMI_LIMIT):02.0f}_00"  
-        end_col = f"DOS_p{abs(config.EFERMI_LIMIT):02.0f}_00"
-        self.dos_cols = self.data.loc[:, start_col:end_col].columns.tolist()
-        self.categorical_cols = ['bravais_lattice']
-        self.numerical_cols = ['fermi_energy', 'is_magnetic']
-        self.target_col = ['is_superconductor']
-
-    def get_X_y_data(self):
-        X = self.data.drop(self.target_col, axis=1).copy()
-        X = X.fillna(0) 
-        y = self.data[self.target_col].copy()
-        y = y.iloc[:, 0]
-        return X, y
-
-    def basic_processing(self, X, y, PCA_cols = None):
-        if PCA_cols == None:
-            all_numerical_cols = self.numerical_cols + self.dos_cols
-        else:
-            all_numerical_cols = self.numerical_cols + PCA_cols
-        categorical_transformer = OneHotEncoder(handle_unknown='ignore')
-        numerical_transformer = RobustScaler()
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('cat', categorical_transformer, self.categorical_cols),
-                ('num', numerical_transformer, all_numerical_cols)  
-            ]
-        )  
-        X_preprocessed = preprocessor.fit_transform(X)
-        feature_names = preprocessor.get_feature_names_out()
-        if hasattr(X_preprocessed, 'todense'):
-            X_preprocessed = X_preprocessed.todense()
-        X_preprocessed_df = pd.DataFrame(X_preprocessed, columns=feature_names)
-        return X_preprocessed_df, y
     
     @staticmethod
     def split_resample_data(X, y, resampling_technique=None, test_size=0.2, random_state=42):
@@ -91,14 +51,6 @@ class HiperparameterExploration:
         if resampling_technique!=None:
             X_train, y_train = resampling_technique.fit_resample(X_train, y_train)
         return X_train, X_test, y_train, y_test
-    
-    def apply_pca(self, n_PCA, X):
-        pca = PCA(n_components=n_PCA)
-        pca_columns = [f'PC{i+1}' for i in range(n_PCA)]
-        X_pca_1 = pca.fit_transform(X[self.dos_cols]) 
-        X_pca_2 = pd.DataFrame(X_pca_1, columns=pca_columns)
-        X_pca = pd.concat([X.loc[:, :'is_magnetic'].reset_index(drop=True), X_pca_2.reset_index(drop=True)], axis=1)
-        return X_pca, pca_columns
 
     @staticmethod
     def run_search_bayes(X, y, model_class, name, search_space, n_iter = 100, scoring = 'f1', cv=5):
@@ -174,10 +126,9 @@ class HiperparameterExploration:
             self.report_eval_metrics(best_param, best_model, X_test, y_test, name, report_path)
             
     def hiperparameter_exploration_workflow(self):
-        self.process_data()
-        self.define_columns()
-        X, y = self.get_X_y_data()
-        X, pca_columns = self.apply_pca(self.n_pca, X)
-        X, y = self.basic_processing(X, y, pca_columns)
+        X, y = self.Data_Processor.preprocess_data()
+        X, pca_columns = self.Data_Processor.apply_pca(self.n_pca, X)
+        X, y = self.Data_Processor.basic_processing(X, y, pca_columns)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         self.run_search(X_train, y_train)
+        self.evaluate_search_method(X_test, y_test)
