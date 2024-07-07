f24521ba4f551643c34dfd98086f075bb54dc7ce
olayafgon <olayafolgueiras@gmail.com>
2024-07-07 15:39:04 +0200

    Documenting

--- src/models/data_model_processing/data_model_processor.py
+++ src/models/data_model_processing/data_model_processor.py
@@ -9,16 +9,38 @@ sys.path.append('./../../')
 import config
 
 class DataPreprocessor:
+    """
+    A class to preprocess the data for machine learning models.
+
+    Attributes:
+        data (pd.DataFrame): The input data.
+        target_column (str): The name of the target column.
+        efermi_limit (float): The Fermi energy limit to define DOS columns.
+    """
     def __init__(self, data):
+        """
+        Constructs all the necessary attributes for the DataPreprocessor object.
+
+        ARGS:
+            data (pd.DataFrame): The input data.
+        """
         self.data = data
         self.target_column = config.TARGET_COLUMN
         self.efermi_limit = config.EFERMI_LIMIT
 
     def process_data(self):
+        """
+        Preprocesses the data by removing NaN values and converting the target column to boolean type.
+
+        """
         self.data.dropna(inplace=True)
         self.data['is_superconductor'] = self.data['is_superconductor'].astype(bool)
 
     def define_columns(self):
+        """
+        Defines the DOS, categorical, numerical, and target columns.
+
+        """
         start_col = f"DOS_m{abs(self.efermi_limit):02.0f}_00"  
         end_col = f"DOS_p{abs(self.efermi_limit):02.0f}_00"
         self.dos_cols = self.data.loc[:, start_col:end_col].columns.tolist()
@@ -27,6 +49,12 @@ class DataPreprocessor:
         self.target_col = [self.target_column]
 
     def get_X_y_data(self):
+        """
+        Separates the data into features (X) and target (y).
+
+        RETURNS:
+            tuple: A tuple containing the features (X) and target (y).
+        """
         X = self.data.drop(self.target_col, axis=1).copy()
         X = X.fillna(0) 
         y = self.data[self.target_col].copy()
@@ -34,12 +62,29 @@ class DataPreprocessor:
         return X, y
     
     def preprocess_data(self):
+        """
+        Preprocesses the data by executing the process_data, define_columns, and get_X_y_data methods.
+
+        RETURNS:
+            tuple: A tuple containing the features (X) and target (y).
+        """
         self.process_data()
         self.define_columns()
         X, y = self.get_X_y_data()
         return X, y
     
     def basic_processing(self, X, y, PCA_cols = None):
+        """
+        Applies basic preprocessing steps to the data, including one-hot encoding for categorical features and robust scaling for numerical features.
+
+        ARGS:
+            X (pd.DataFrame): The features data.
+            y (pd.Series): The target data.
+            PCA_cols (list, optional): A list of PCA column names. Defaults to None.
+
+        RETURNS:
+            tuple: A tuple containing the processed features (X_preprocessed_df) and target (y).
+        """
         if PCA_cols == None:
             all_numerical_cols = self.numerical_cols + self.dos_cols
         else:
@@ -60,6 +105,16 @@ class DataPreprocessor:
         return X_preprocessed_df, y
 
     def apply_pca(self, n_PCA, X):
+        """
+        Applies PCA to the DOS columns.
+
+        ARGS:
+            n_PCA (int): The number of PCA components.
+            X (pd.DataFrame): The features data.
+
+        RETURNS:
+            tuple: A tuple containing the PCA-transformed features (X_pca) and the list of PCA column names (pca_columns).
+        """
         pca = PCA(n_components=n_PCA)
         pca_columns = [f'PC{i+1}' for i in range(n_PCA)]
         X_pca_1 = pca.fit_transform(X[self.dos_cols]) 
