1b36e47bb4880ef1de58c7761e2c2fa1a00c665b
olayafgon <olayafolgueiras@gmail.com>
2024-07-10 01:00:19 +0200

    Added plots

--- src/models/final_model/model_evaluation.py
+++ src/models/final_model/model_evaluation.py
@@ -1,6 +1,9 @@
 import sys
 import os
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import shap
 
 sys.path.append('./../')
 import config
@@ -22,6 +25,9 @@ class ModelEvaluation:
         self.model_pca = config.FINAL_MODEL_PCA_NUMBER
         self.model_resampling = config.FINAL_MODEL_RESAMPLER
         tools.create_folder(self.eval_folder_path)
+        mpl.rcParams['font.family'] = 'serif'
+        mpl.rcParams['font.serif'] = ['Times New Roman'] 
+        mpl.rcParams['text.usetex'] = False
         
     def evaluate(self):
         self.y_pred = self.model.predict(self.X_test)
@@ -45,8 +51,42 @@ class ModelEvaluation:
             file.write(f"F1-score: {f1:.4f}\n")
             file.write(f"AUC: {auc:.4f}\n")
             file.write(f"Matriz de confusión:\n{confusion_matrix_result}\n")
+    
+    @staticmethod     
+    def plot_training_curve(model, save_path):
+        results = model.evals_result()
+        epochs = len(results['validation_0']['error'])
+        plt.figure(figsize=(4, 4))
+        plt.plot(range(epochs), results['validation_0']['error'], label='Error de entrenamiento')
+        plt.plot(range(epochs), results['validation_1']['error'], label='Error de validación')
+        plt.xlabel('epochs', fontsize=11)
+        plt.ylabel('Error', fontsize=11)
+        plt.title('Evolución del entrenamiento', fontsize=13)
+        plt.xticks(fontsize=9)
+        plt.yticks(fontsize=9)
+        plt.legend()
+        plt.savefig(os.path.join(save_path, 'curva_entrenamiento.png'))
+        plt.close()
+        
+    @staticmethod     
+    def plot_shap_values(model, X_test, save_path):
+        explainer = shap.Explainer(model, X_test)
+        shap_values = explainer(X_test)
+        fig = shap.plots.bar(shap_values, max_display=13, show=False)
+        plt.savefig(os.path.join(save_path, 'shap_barras.png'))
+        fig = shap.plots.beeswarm(shap_values, max_display=15, show=False)
+        plt.savefig(os.path.join(save_path, 'shap_enjambre.png'))
+        
+    def plot_figures(self):
+        figures_save_path = os.path.join(self.eval_folder_path, 'plots')
+        tools.create_folder(figures_save_path)
+        if self.model_algorithm == 'XGBClassifier':
+            self.plot_training_curve(self.model, figures_save_path)
+        self.plot_shap_values(self.model, self.X_test, figures_save_path)
+        
         
     def model_training_run(self):
         self.evaluate()
         self.calculate_save_metrics()
+        self.plot_figures()
         
