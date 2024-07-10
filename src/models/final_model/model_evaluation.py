3923cf6d788569c45649955315f3c55f3304ebe2
olayafgon <olayafolgueiras@gmail.com>
2024-07-10 19:48:11 +0200

    More plots

--- src/models/final_model/model_evaluation.py
+++ src/models/final_model/model_evaluation.py
@@ -12,7 +12,6 @@ sys.path.append('./../')
 import config
 from utils import tools
 
-
 class ModelEvaluation:
     def __init__(self, run_results_path, model, X_test, y_test, data_test, supercon_data):
         self.run_results_path = run_results_path
@@ -101,7 +100,6 @@ class ModelEvaluation:
         data2_counts = data2['is_magnetic'].value_counts().sort_index().reindex([0, 1], fill_value=0)
         total_counts1 = data1_counts.sum()
         total_counts2 = data2_counts.sum()
-
         labels = ['No', 'Sí']
         x = range(len(labels))
         bar_width = 0.35
@@ -117,7 +115,6 @@ class ModelEvaluation:
             yval = bar.get_height()
             percent = yval / total_counts2 * 100
             plt.text(bar.get_x() + bar.get_width() / 2, yval, f'{int(yval)}\n({percent:.1f}%)', va='bottom', ha='center')
-
         plt.ylim(0, max_height * 1.25)
         plt.xlabel('¿Es magnético?')
         plt.ylabel('Frecuencia')
@@ -145,6 +142,92 @@ class ModelEvaluation:
         ax.set_xticks(x)
         ax.set_xticklabels(combined_categories, rotation=90)
         ax.legend()
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_path, save_name + '.png'))
+        plt.close()
+    
+    @staticmethod
+    def plot_corr_matrix(data1, data2, label1, label2, title, save_path, save_name):
+        data1_subset = data1[['fermi_energy', 'is_magnetic', 'critical_temperature_k']].copy()
+        data2_subset = data2[['fermi_energy', 'is_magnetic', 'critical_temperature_k']].copy()
+        corr_matrix_data1 = data1_subset.corr()
+        corr_matrix_data2 = data2_subset.corr()
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+        im1 = ax1.imshow(corr_matrix_data1, cmap='coolwarm', interpolation='nearest', vmin=-1, vmax=1)
+        cbar = ax1.figure.colorbar(im1, ax=ax1)
+        ax1.set_xticks(np.arange(len(corr_matrix_data1.columns)))
+        ax1.set_yticks(np.arange(len(corr_matrix_data1.columns)))
+        ax1.set_xticklabels(corr_matrix_data1.columns, rotation=45, ha="right")
+        ax1.set_yticklabels(corr_matrix_data1.columns)
+        for i in range(len(corr_matrix_data1.columns)):
+            for j in range(len(corr_matrix_data1.columns)):
+                text = ax1.text(j, i, f"{corr_matrix_data1.iloc[i, j]:.2f}", ha="center", va="center", color="black", size=8)
+        ax1.set_title(f'Correlation Matrix - {label1}')
+        im2 = ax2.imshow(corr_matrix_data2, cmap='coolwarm', interpolation='nearest', vmin=-1, vmax=1)
+        cbar = ax2.figure.colorbar(im2, ax=ax2)
+        ax2.set_xticks(np.arange(len(corr_matrix_data2.columns)))
+        ax2.set_yticks(np.arange(len(corr_matrix_data2.columns)))
+        ax2.set_xticklabels(corr_matrix_data2.columns, rotation=45, ha="right")
+        ax2.set_yticklabels(corr_matrix_data2.columns)
+        for i in range(len(corr_matrix_data2.columns)):
+            for j in range(len(corr_matrix_data2.columns)):
+                text = ax2.text(j, i, f"{corr_matrix_data2.iloc[i, j]:.2f}", ha="center", va="center", color="black", size=8)
+        ax2.set_title(f'Correlation Matrix - {label2}')
+        plt.suptitle(title)
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_path, save_name + '.png'))
+        plt.close()
+    
+    @staticmethod
+    def plot_median_dos_superposed_4(data_materials1, data_materials2, data_materials3, data_materials4, title1, title2, title3, title4, combined_title, save_path, save_name):
+        data_plot1 = data_materials1.copy()
+        data_plot2 = data_materials2.copy()
+        data_plot3 = data_materials3.copy()
+        data_plot4 = data_materials4.copy()
+        plt.figure(figsize=(8,4))
+        average_dos1 = data_plot1.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)  
+        average_dos2 = data_plot2.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)  
+        average_dos3 = data_plot3.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)  
+        average_dos4 = data_plot4.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)
+        plt.plot(average_dos1, label=title1)
+        plt.plot(average_dos2, label=title2)
+        plt.plot(average_dos3, label=title3)
+        plt.plot(average_dos4, label=title4)
+        num_cols = len(data_materials1.columns)
+        center_index = num_cols // 2 
+        plt.axhline(y=0, color='black', linestyle=':')
+        plt.axvline(x=center_index, color='black', linestyle=':') 
+        plt.xticks([0, center_index, num_cols - 1], labels=['$E_f-15eV$', '0', '$E_f+15eV$'])
+        plt.ylabel("Density of States (DOS)")
+        plt.xlabel("Energía - Energía de Fermi")
+        plt.title(combined_title)
+        plt.grid(False)
+        plt.legend()
+        plt.ylim(-0.5, 8.5)
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_path, save_name + '.png'))
+        plt.close()
+        
+    @staticmethod
+    def plot_median_dos_superposed_2(data_materials1, data_materials2, title1, title2, combined_title, save_path, save_name):
+        data_plot1 = data_materials1.copy()
+        data_plot2 = data_materials2.copy()
+        plt.figure(figsize=(8,4))
+        average_dos1 = data_plot1.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)  
+        average_dos2 = data_plot2.loc[:, 'DOS_m15_00':'DOS_p15_00'].median(axis=0)  
+        plt.plot(average_dos1, label=title1)
+        plt.plot(average_dos2, label=title2)
+        num_cols = len(data_materials1.columns)
+        center_index = num_cols // 2 
+        plt.axhline(y=0, color='black', linestyle=':')
+        plt.axvline(x=center_index, color='black', linestyle=':') 
+        plt.xticks([0, center_index, num_cols - 1], labels=['$E_f-15eV$', '0', '$E_f+15eV$'])
+        plt.ylabel("Density of States (DOS)")
+        plt.xlabel("Energía - Energía de Fermi")
+        plt.title(combined_title)
+        plt.grid(False)
+        plt.legend()
+        plt.ylim(-0.5, 8.5)
         plt.tight_layout()
         plt.savefig(os.path.join(save_path, save_name + '.png'))
         plt.close()
@@ -185,6 +268,24 @@ class ModelEvaluation:
         self.plot_bravais_lattice_distribution(self.false_positive, self.true_negative, 'Falsos positivos', 'Verdaderos negativos', 
                                         'Red de Bravais en predicciones de no superconductores', 
                                         figures_save_path, 'bravais_lattice_non_supercond_predictions')
+        self.plot_corr_matrix(self.true_positive, self.false_negative, 'Verdaderos positivos', 'Falsos negativos', 
+                                        'Correlaciones en predicciones de superconductores', 
+                                        figures_save_path, 'correlations_supercond_predictions')
+        self.plot_corr_matrix(self.false_positive, self.true_negative, 'Falsos positivos', 'Verdaderos negativos', 
+                                        'Correlaciones en predicciones de no superconductores', 
+                                        figures_save_path, 'correlations_non_supercond_predictions')
+        self.plot_median_dos_superposed_4(self.true_positive, self.false_negative, self.true_negative, self.false_positive,  
+                                        'Mediana verdaderos positivos', 'Mediana falsos negativos', 
+                                        'Mediana verdaderos negativos', 'Mediana falsos positivos',
+                                        'Medianas de la DOS',
+                                        figures_save_path, 'dos_median_all_predictions')
+        self.plot_median_dos_superposed_2(self.true_positive, self.false_negative, 'Verdaderos positivos', 'Falsos negativos', 
+                                        'Medianas de la DOS en predicciones de superconductores', 
+                                        figures_save_path, 'dos_median_supercond_predictions')
+        self.plot_median_dos_superposed_2(self.false_positive, self.true_negative, 'Falsos positivos', 'Verdaderos negativos',  
+                                        'Medianas de la DOS en predicciones de no superconductores', 
+                                        figures_save_path, 'dos_median_non_supercond_predictions')
+
 
     def model_evaluation_run(self):
         self.evaluate()
